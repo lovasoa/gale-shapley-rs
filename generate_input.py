68a0188897d5f6b0c7070f4390c7c985eb41575a
lovasoa <pere.jobs@gmail.com>
2023-05-06 03:31:45 +0200

    improve input generation script

--- generate_input.py
+++ generate_input.py
@@ -1,4 +1,10 @@
-N=10000
+"""
+Usage: python3 generate_input.py 100 > input.txt
+./galeshapley < input.txt
+"""
+import sys
+
+N=int(sys.argv[1])
 for i in range(2*N):
     print(i%N, end=': ')
     for j in range(N):
